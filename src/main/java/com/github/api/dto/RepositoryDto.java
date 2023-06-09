92571801f9b87dfc0087fd8b69b09dbccfd8e323
kubabukry <95041821+kubabukry@users.noreply.github.com>
2023-06-09 18:09:04 +0200

    Getting repos filters those which are forks

--- src/main/java/com/github/api/dto/RepositoryDto.java
+++ src/main/java/com/github/api/dto/RepositoryDto.java
@@ -1,14 +1,12 @@
 package com.github.api.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class RepositoryDto {
     private String name;
     private String login;
--- src/main/java/com/github/api/service/GithubService.java
+++ src/main/java/com/github/api/service/GithubService.java
@@ -3,6 +3,7 @@ package com.github.api.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.api.dto.BranchDto;
 import com.github.api.dto.RepositoryDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,7 +14,6 @@ import java.util.List;
 
 @Service
 public class GithubService {
-
     private final WebClient webClient;
 
     private final ObjectMapper objectMapper;
@@ -35,11 +35,11 @@ public class GithubService {
     private Flux<RepositoryDto> mapToRepositoryDto(String repoJson, String username) {
         try {
             JsonNode jsonNode = objectMapper.readTree(repoJson);
-            List<JsonNode> repoNodes = jsonNode.findValues("name");
 
-            return Flux.fromIterable(repoNodes)
+            return Flux.fromIterable(jsonNode)
+                    .filter(node -> node.has("fork") && !node.get("fork").asBoolean())
                     .map(node -> {
-                        String repoName = node.asText();
+                        String repoName = node.get("name").asText();
                         RepositoryDto repositoryDto = new RepositoryDto();
                         repositoryDto.setName(repoName);
                         repositoryDto.setLogin(username);
@@ -49,4 +49,34 @@ public class GithubService {
             return Flux.error(e);
         }
     }
+
+
+    public Mono<List<BranchDto>> getAllBranches(String username, String repo){
+        return webClient.get()
+                .uri("/repos/"+username+"/"+repo+"/branches")
+                .retrieve()
+                .bodyToFlux(String.class)
+                .flatMap(branchJson -> mapToBranchDto(branchJson))
+                .collectList();
+    }
+
+private Flux<BranchDto> mapToBranchDto(String branchJson) {
+    try {
+        JsonNode jsonNode = objectMapper.readTree(branchJson);
+
+        return Flux.fromIterable(jsonNode)
+                .map(node -> {
+                    String branchName = node.get("name").asText();
+                    String lastCommitSha = node.get("commit").get("sha").asText();
+
+                    BranchDto branchDto = new BranchDto();
+                    branchDto.setName(branchName);
+                    branchDto.setLastCommitSha(lastCommitSha);
+
+                    return branchDto;
+                });
+    } catch (JsonProcessingException e) {
+        return Flux.error(e);
+    }
+}
 }
